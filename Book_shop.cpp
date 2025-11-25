9047909c48d1dc69cdc43f10323401e7f3a00d7f
Sally <shinthantaein320@gmail.com>
2025-11-25 20:14:44 +0700

    first change

--- Book_shop.cpp
+++ Book_shop.cpp
@@ -1,90 +1,85 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
-double cm_to_inch(int);
-double pound_to_gram(int);
-double surface_area(int, int);
-double discount_for_instockday(int);
+
+double cm_to_inch(double value);
+double pound_to_gram(double value);
+double surface_area(double w, double h);
+double discount_for_instockday(int instock_day);
 double one_book_price();
 
-int main(){
+int main() {
 
     int books;
-    double total_price =0;
-    double price, cheapest_price;
+    double total_price = 0;
 
     cout << "Enter number of books : ";
     cin >> books;
 
-    for(int i=0; i<books; i++){
-        price = one_book_price();
+    for(int i = 0; i < books; i++){
+        double price = one_book_price();
         total_price += price;
     }
 
-
+    cout << "Total price = " << total_price << endl;
 
     return 0;
 }
 
-// //******* Unit_Convertion ********//
+double cm_to_inch(double value){
+    return value / 2.54;
+}
 
-// double cm_to_inch(int value){
-//     return value/ 2.54;
-// }
+double pound_to_gram(double value){
+    return value * 453.592;
+}
 
-// double pound_to_gram(int value){
-//     return value * 453.592;
-// }
+double surface_area(double w, double h){
+    return w * h;
+}
 
-// //******* Surface_Area_Calculatio *******//
+double discount_for_instockday(int instock_day){
+    double discount = 0;
 
-// double surface_area(int w, int h){
-//     return w * h;
-// }
+    if(instock_day > 20){
+        discount = (10 * 2) + ((instock_day - 20) * 5);
+    }
+    else if(instock_day > 10){
+        discount = (instock_day - 10) * 2;
+    }
+    else {
+        discount = 0;
+    }
 
-// //******** Calculating_Discount *******//
+    return discount;
+}
 
-// double discount_for_instockday(int instock_day){
-//     int discount = 0;
+double one_book_price(){
 
-//     if(instock_day > 10 ){
-//         discount = (instock_day - 10) * 2;
-//     } else if(instock_day > 20) {
-//         discount = (10*2) + ((instock_day - 20 ) * 5);
-//     }else {
-//         discount = 0;
-//     }
+    double w, h, weight;
+    int pages, instockday;
 
-// return discount;
+    cout << "Enter book width (cm): ";
+    cin >> w;
+    w = cm_to_inch(w);
 
-// }
+    cout << "Enter book height (cm): ";
+    cin >> h;
+    h = cm_to_inch(h);
 
-// //****** One Book Price *******//
+    cout << "Enter book weight (pounds): ";
+    cin >> weight;
+    weight = pound_to_gram(weight);
 
-// double one_book_price(){
+    cout << "Enter number of pages: ";
+    cin >> pages;
 
-//     int w,h, weight, pages, instockday;
-//     cout << "Enter book width : ";
-//     cin >> w;
-//     w = cm_to_inch(w);
+    cout << "Enter number of instock days: ";
+    cin >> instockday;
 
-//     cout << "Enter book height : ";
-//     cin >> h;
-//     h = cm_to_inch(h);
+    double SA = surface_area(w, h);
 
-//     cout << "Enter book weight : ";
-//     cin >> weight;
-//     weight = pound_to_gram(weight);
+    double price = SA * weight * 10 + sqrt(pages) - discount_for_instockday(instockday);
 
-//     cout << "Enter number of pages : ";
-//     cin >> pages;
-
-//     cout << "Enter number of instock days :  ";
-//     cin >> instockday;
-
-//     double SA = surface_area(w,h);
-
-//     double price = SA * weight * 10 + sqrt(pages) - discount_for_instockday(instockday);
-
-//     return price;
-// }
+    return price;
+}
